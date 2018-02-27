de9380843ced3dfb906db86646bc7c0afa0a5ef2
Nathaniel SEYLER <nathaniel.seyler@etu.unistra.fr>
2018-02-27 17:47:14 +0100

    ajout des structures

---
+++ src/hashcode.h
@@ -0,0 +1,29 @@
+typedef struct Video {
+	int id;
+	int length;
+}Video;
+
+typedef struct Cache {
+	int id;
+	int capacity;
+	Video *tab;
+}Cache;
+
+typedef struct Couple {
+	int latence;
+	Cache cache;
+}Couple;
+
+typedef struct EndPoint {
+	int id;
+	Couple *tab;
+	int latence_datacenter;
+}EndPoint;
+
+int init_video(int id, int length);
+int init_cache(int id, int length);
+int init_ep(int id, int length);
+
+int lecture(const char *input);
+int ecriture(const char *input);
+Cache min(Couple * tab);
