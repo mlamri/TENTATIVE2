2a9b7f60ee54e9a9196ae2f40f367c64df5ae204
Nathaniel SEYLER <nathaniel.seyler@etu.unistra.fr>
2018-03-01 00:56:56 +0100

    fdsfsqdvgrsf

--- src/hashcode.h
+++ src/hashcode.h
@@ -32,11 +32,20 @@ typedef struct Request{
 	int requests;
 }Request;
 
+
+typedef struct File_data{
+    int v;
+    int e;
+    int r;
+    int c;
+    int x;
+} File_data;
+
 int init_video(int id, int length);
 int init_cache(int id, int length);
 int init_ep(int id, int length);
 
-int lecture(const char *input);
+int lecture(const char *input,File_data *fd,Cache *tab_c,Video * dc,EndPoint * tab_ep,Request *tab_r );
 int ecriture(const char *input);
 Cache min(Couple * tab);
 
